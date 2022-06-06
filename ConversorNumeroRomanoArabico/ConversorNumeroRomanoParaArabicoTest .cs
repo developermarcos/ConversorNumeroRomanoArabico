c09ee0a6848d589cee679008ad8f964c3048d489
Marcos Lima <developermarcos@outlook.com>
2022-06-06 15:45:57 -0300

    Gerador de numeros finalizado

--- ConversorNumeroRomanoArabico/ConversorNumeroRomanoParaArabicoTest .cs
+++ ConversorNumeroRomanoArabico/ConversorNumeroRomanoParaArabicoTest .cs
@@ -62,6 +62,271 @@ namespace ConversorNumero.Teste
         }
 
         [TestMethod]
+        [DataRow("XXI", 21)]
+        [DataRow("XXII", 22)]
+        [DataRow("XXIII", 23)]
+        [DataRow("XXIV", 24)]
+        [DataRow("XXV", 25)]
+        [DataRow("XXVI", 26)]
+        [DataRow("XXVII", 27)]
+        [DataRow("XXVIII", 28)]
+        [DataRow("XXIX", 29)]
+        [DataRow("XXX", 30)]
+        public void Deve_converter_romanos_numeros_do_21_ao_30(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversorNumeroRomanoParaArabico.ConverterRomanosParaNumerico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("XXXI", 31)]
+        [DataRow("XXXII", 32)]
+        [DataRow("XXXIII", 33)]
+        [DataRow("XXXIV", 34)]
+        [DataRow("XXXV", 35)]
+        [DataRow("XXXVI", 36)]
+        [DataRow("XXXVII", 37)]
+        [DataRow("XXXVIII", 38)]
+        [DataRow("XXXIX", 39)]
+        [DataRow("XL", 40)]
+        public void Deve_converter_romanos_numeros_do_31_ao_40(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversorNumeroRomanoParaArabico.ConverterRomanosParaNumerico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("XLI", 41)]
+        [DataRow("XLII", 42)]
+        [DataRow("XLIII", 43)]
+        [DataRow("XLIV", 44)]
+        [DataRow("XLV", 45)]
+        [DataRow("XLVI", 46)]
+        [DataRow("XLVII", 47)]
+        [DataRow("XLVIII", 48)]
+        [DataRow("XLIX", 49)]
+        [DataRow("L", 50)]
+        public void Deve_converter_romanos_numeros_do_41_ao_50(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversorNumeroRomanoParaArabico.ConverterRomanosParaNumerico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("XCI", 91)]
+        [DataRow("XCII", 92)]
+        [DataRow("XCIII", 93)]
+        [DataRow("XCIV", 94)]
+        [DataRow("XCV", 95)]
+        [DataRow("XCVI", 96)]
+        [DataRow("XCVII", 97)]
+        [DataRow("XCVIII", 98)]
+        [DataRow("XCIX", 99)]
+        [DataRow("C", 100)]
+        public void Deve_converter_romanos_numeros_do_91_ao_100(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversorNumeroRomanoParaArabico.ConverterRomanosParaNumerico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("LI", 51)]
+        [DataRow("LII", 52)]
+        [DataRow("LIII", 53)]
+        [DataRow("LIV", 54)]
+        [DataRow("LV", 55)]
+        [DataRow("LVI", 56)]
+        [DataRow("LVII", 57)]
+        [DataRow("LVIII", 58)]
+        [DataRow("LIX", 59)]
+        [DataRow("LX", 60)]
+        public void Deve_converter_romanos_numeros_do_51_ao_60(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversorNumeroRomanoParaArabico.ConverterRomanosParaNumerico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("LXI", 61)]
+        [DataRow("LXII", 62)]
+        [DataRow("LXIII", 63)]
+        [DataRow("LXIV", 64)]
+        [DataRow("LXV", 65)]
+        [DataRow("LXVI", 66)]
+        [DataRow("LXVII", 67)]
+        [DataRow("LXVIII", 68)]
+        [DataRow("LXIX", 69)]
+        [DataRow("LXX", 70)]
+        public void Deve_converter_romanos_numeros_do_61_ao_70(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversorNumeroRomanoParaArabico.ConverterRomanosParaNumerico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("LXXI", 71)]
+        [DataRow("LXXII", 72)]
+        [DataRow("LXXIII", 73)]
+        [DataRow("LXXIV", 74)]
+        [DataRow("LXXV", 75)]
+        [DataRow("LXXVI", 76)]
+        [DataRow("LXXVII", 77)]
+        [DataRow("LXXVIII", 78)]
+        [DataRow("LXXIX", 79)]
+        [DataRow("LXXX", 80)]
+        public void Deve_converter_romanos_numeros_do_71_ao_80(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversorNumeroRomanoParaArabico.ConverterRomanosParaNumerico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("LXXXI", 81)]
+        [DataRow("LXXXII", 82)]
+        [DataRow("LXXXIII", 83)]
+        [DataRow("LXXXIV", 84)]
+        [DataRow("LXXXV", 85)]
+        [DataRow("LXXXVI", 86)]
+        [DataRow("LXXXVII", 87)]
+        [DataRow("LXXXVIII", 88)]
+        [DataRow("LXXXIX", 89)]
+        [DataRow("XC", 90)]
+        public void Deve_converter_romanos_numeros_do_81_ao_90(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversorNumeroRomanoParaArabico.ConverterRomanosParaNumerico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        //[TestMethod]
+        //[DataRow("CI", 101)]
+        //[DataRow("CII", 102)]
+        //[DataRow("CIII", 103)]
+        //[DataRow("CIV", 104)]
+        //[DataRow("CV", 105)]
+        //[DataRow("CVI", 106)]
+        //[DataRow("CVII", 107)]
+        //[DataRow("CVIII", 108)]
+        //[DataRow("CIX", 109)]
+        //[DataRow("CX", 110)]
+        //public void Deve_converter_romanos_numeros_do_101_ao_110(string numero, int resultadoEsperado)
+        //{
+        //    string numeroRomano = numero;
+
+        //    //ação - action 
+        //    var resultado = conversorNumeroRomanoParaArabico.ConverterRomanosParaNumerico(numeroRomano);
+
+        //    //verificação - assert
+        //    Assert.AreEqual(resultadoEsperado, resultado);
+        //}
+
+        [TestMethod]
+        [DataRow("CXI", 111)]
+        [DataRow("CXXIII", 123)]
+        [DataRow("CXXXV", 135)]
+        [DataRow("CXLVII", 147)]
+        [DataRow("CLVIII", 158)]
+        [DataRow("CLXIX", 169)]
+        [DataRow("CLXXI", 171)]
+        [DataRow("CLXXXII", 182)]
+        [DataRow("CXCIII", 193)]
+        [DataRow("CC", 200)]
+        public void Deve_converter_romanos_numeros_entre_111_e_200(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversorNumeroRomanoParaArabico.ConverterRomanosParaNumerico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("CCI", 201)]
+        [DataRow("CMXCIX", 999)]
+        [DataRow("CCCLVII", 357)]
+        [DataRow("CDLVI", 456)]
+        [DataRow("DLXXXII", 582)]
+        [DataRow("DCXXXIX", 639)]
+        [DataRow("DCCXLI", 741)]
+        [DataRow("DCCCXLVI", 846)]
+        [DataRow("CMXXX", 930)]
+        [DataRow("CDLVIII", 458)]
+        public void Deve_converter_romanos_numeros_entre_201_e_999(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversorNumeroRomanoParaArabico.ConverterRomanosParaNumerico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("M", 1000)]
+        [DataRow("MMMCMXCIX", 3999)]
+        [DataRow("MMCXXVII", 2127)]
+        [DataRow("MDCCLVIII", 1758)]
+        [DataRow("MMDCLXXXIV", 2684)]
+        [DataRow("MMMCCXLVIII", 3248)]
+        [DataRow("MDCLXXXVII", 1687)]
+        [DataRow("MMMCCX", 3210)]
+        [DataRow("MCDXCVI", 1496)]
+        [DataRow("MMDCCCLXXIX", 2879)]
+        public void Deve_converter_romanos_numeros_entre_1000_e_3999(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversorNumeroRomanoParaArabico.ConverterRomanosParaNumerico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+
+        [TestMethod]
         [DataRow(1,"I")]
         [DataRow(2,"II")]
         [DataRow(3,"III")]
--- ConversorNumeroRomanoParaArabico/ConversorNumeroRomanoParaArabico.cs
+++ ConversorNumeroRomanoParaArabico/ConversorNumeroRomanoParaArabico.cs
@@ -38,18 +38,214 @@ namespace ConversorNumero.Dominio
             numeroArabicos.Add("DCCC", 800);
             numeroArabicos.Add("CM", 900);
             numeroArabicos.Add("M", 1000);
-
+            numeroArabicos.Add("MM", 2000);
+            numeroArabicos.Add("MMM", 3000);
         }
 
         public int ConverterRomanosParaNumerico(string numeroRomano)
         {
+            string[] numerosRomanosArray = numeroRomano.Split("");
+            int numeroConvertido = 0;
+
             if (numeroRomano.StartsWith("X"))
             {
-                numeroRomano = numeroRomano.Substring(1);
-                return numeroArabicos[numeroRomano] + 10;
+                numeroConvertido = IniciaX(numeroRomano);
+            }
+            else if (numeroRomano.StartsWith("L"))
+            {
+                numeroConvertido = IniciaL(numeroRomano);
+            }
+            else if (numeroRomano.StartsWith("C"))
+            {
+                numeroConvertido = IniciaC(numeroRomano);
+            }
+            else if (numeroRomano.StartsWith("D"))
+            {
+                numeroConvertido = IniciaD(numeroRomano);
+            }
+            else if (numeroRomano.StartsWith("M"))
+            {
+                numeroConvertido = IniciaM(numeroRomano);
+            }
+            else
+            {
+                numeroConvertido = numeroArabicos[numeroRomano];
             }
 
-            return numeroArabicos[numeroRomano];
+            return numeroConvertido;
+        }
+
+        private int IniciaX(string numeroRomano)
+        {
+            int numeroConvertido = 0;
+
+            string numeroRomanoUnidade = "";
+
+            if (numeroRomano.StartsWith("XXX"))
+            {
+                numeroRomanoUnidade = numeroRomano.Substring(3);
+                numeroConvertido = 30;
+            }
+
+            else if (numeroRomano.StartsWith("XX"))
+            {
+                numeroRomanoUnidade = numeroRomano.Substring(2);
+                numeroConvertido = 20;
+            }
+
+            else if (numeroRomano.StartsWith("XL"))
+            {
+
+                numeroRomanoUnidade = numeroRomano.Substring(2);
+                numeroConvertido = 40;
+            }
+
+            else if (numeroRomano.StartsWith("XC"))
+            {
+
+                numeroRomanoUnidade = numeroRomano.Substring(2);
+                numeroConvertido = 90;
+            }
+
+            else if (numeroRomano.StartsWith("X"))
+            {
+
+                numeroRomanoUnidade = numeroRomano.Substring(1);
+                numeroConvertido = 10;
+            }
+
+            numeroConvertido += numeroArabicos[numeroRomanoUnidade];
+
+            return numeroConvertido;
+        }
+        private int IniciaL(string numeroRomano)
+        {
+            int numeroConvertido = 0;
+            string numeroRomanoUnidade = "";
+
+            if (numeroRomano.StartsWith("LXXX"))
+            {
+                numeroRomanoUnidade = numeroRomano.Substring(4);
+                numeroConvertido = 80;
+            }
+
+            else if (numeroRomano.StartsWith("LXX"))
+            {
+                numeroRomanoUnidade = numeroRomano.Substring(3);
+                numeroConvertido = 70;
+            }
+
+            else if (numeroRomano.StartsWith("LX"))
+            {
+                numeroRomanoUnidade = numeroRomano.Substring(2);
+                numeroConvertido = 60;
+            }
+
+            else if (numeroRomano.StartsWith("L"))
+            {
+                numeroRomanoUnidade = numeroRomano.Substring(1);
+                numeroConvertido = 50;
+            }
+
+            numeroConvertido += numeroArabicos[numeroRomanoUnidade];
+            return numeroConvertido;
+        }
+        private int IniciaC(string numeroRomano)
+        {
+            int numeroConvertido = 0;
+            string numeroRomanoDecimal = "";
+
+            if (numeroRomano.StartsWith("CCC"))
+            {
+                numeroRomanoDecimal = numeroRomano.Substring(3);
+                numeroConvertido = 300;
+            }
+
+            else if (numeroRomano.StartsWith("CC"))
+            {
+                numeroRomanoDecimal = numeroRomano.Substring(2);
+                numeroConvertido = 200;
+            }
+
+            else if (numeroRomano.StartsWith("CD"))
+            {
+                numeroRomanoDecimal = numeroRomano.Substring(2);
+                numeroConvertido = 400;
+            }
+
+            else if (numeroRomano.StartsWith("CM"))
+            {
+                numeroRomanoDecimal = numeroRomano.Substring(2);
+                numeroConvertido = 900;
+            }
+
+            else if (numeroRomano.StartsWith("C"))
+            {
+                numeroRomanoDecimal = numeroRomano.Substring(1);
+                numeroConvertido = 100;
+            }
+            numeroConvertido += ConverterRomanosParaNumerico(numeroRomanoDecimal);
+            return numeroConvertido;
+        }
+        private int IniciaD(string numeroRomano)
+        {
+            int numeroConvertido = 0;
+            string numeroRomanoDecimal = "";
+
+            if (numeroRomano.StartsWith("DCCC"))
+            {
+                numeroRomanoDecimal = numeroRomano.Substring(4);
+                numeroConvertido = 800;
+            }
+
+            else if (numeroRomano.StartsWith("DCC"))
+            {
+                numeroRomanoDecimal = numeroRomano.Substring(3);
+                numeroConvertido = 700;
+            }
+
+            else if (numeroRomano.StartsWith("DC"))
+            {
+                numeroRomanoDecimal = numeroRomano.Substring(2);
+                numeroConvertido = 600;
+            }
+
+            else if (numeroRomano.StartsWith("D"))
+            {
+                numeroRomanoDecimal = numeroRomano.Substring(1);
+                numeroConvertido = 500;
+            }
+
+            numeroConvertido += ConverterRomanosParaNumerico(numeroRomanoDecimal);
+            
+            return numeroConvertido;
+        }
+        private int IniciaM(string numeroRomano)
+        {
+            int numeroConvertido = 0;
+            string numeroRomanoDecimal = "";
+
+            if (numeroRomano.StartsWith("MMM"))
+            {
+                numeroRomanoDecimal = numeroRomano.Substring(3);
+                numeroConvertido = 3000;
+            }
+
+            else if (numeroRomano.StartsWith("MM"))
+            {
+                numeroRomanoDecimal = numeroRomano.Substring(2);
+                numeroConvertido = 2000;
+            }
+
+            else if (numeroRomano.StartsWith("M"))
+            {
+                numeroRomanoDecimal = numeroRomano.Substring(1);
+                numeroConvertido = 1000;
+            }
+
+            numeroConvertido += ConverterRomanosParaNumerico(numeroRomanoDecimal);
+
+            return numeroConvertido;
         }
     }
 }
